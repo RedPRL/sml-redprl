a0bd55cd9f13483694a1635c6462d0bebabc0694
Jonathan Sterling <jon@jonmsterling.com>
2016-04-22 12:44:52 -0700

    remove fixed heap size, which caused build failures for me

--- script/mlton.sh
+++ script/mlton.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir -p bin
-mlton @MLton fixed-heap 0.5g -- -mlb-path-map mlb-path-map -output bin/jonprl src/frontend.mlb
+mlton -mlb-path-map mlb-path-map -output bin/jonprl src/frontend.mlb
