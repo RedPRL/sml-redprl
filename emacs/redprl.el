987e8bfea05eaeb5e7e578cb8fd1231d2eb2a8fa
David Raymond Christiansen <david@davidchristiansen.dk>
2016-08-15 22:11:45 -0400

    Emacs mode: add the Commentary section
    This text shows up in places like a package overview.

--- emacs/redprl.el
+++ emacs/redprl.el
@@ -20,7 +20,15 @@
 
 ;;; Commentary:
 
+;; This is a major mode for editing RedPRL developments.  The current
+;; editing features include simple syntax highlighting, imenu, and
+;; completion support.  Additionally, there is a command to run RedPRL
+;; in a compilation buffer.
 ;;
+;; RedPRL can be obtained from https://github.com/RedPRL/sml-redprl .
+;;
+;; Make sure to set the variable `redprl-command' to the location of the
+;; redprl binary.
 
 ;;; Code:
 
