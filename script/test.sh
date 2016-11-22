5d8ea475abea47d3bfcf7c4c8ba7ae6c78513408
James Wilcox <jrw12@cs.washington.edu>
2016-11-21 16:27:38 -0800

    make script/test.sh execute test suite

--- script/test.sh
+++ script/test.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+REDPRL=./bin/redprl
+PROBLEM=0
 
-./script/mlton.sh
-./bin/redprl test/test.prl
+echo "Building RedPRL..."
+./script/mlton.sh >/dev/null 2>&1 || { echo "build failed"; exit 1; }
+echo "done!"
+
+# Ensure that the examples file and all suceeding tests succeed
+for f in test/examples.prl test/success/*.prl ; do
+    if ! $REDPRL $f >$f.log 2>&1 ; then
+        PROBLEM=1
+        echo "Test $f should succeed but failed!"
+    fi
+done
+
+
+# Ensure that failing tests fail
+for f in test/failure/*.prl ; do
+    if $REDPRL $f >$f.log 2>&1 ; then
+        PROBLEM=1
+        echo "Test $f should fail but succeeded!"
+    fi
+done
+
+if [ $PROBLEM -eq 0 ] ; then
+    echo "All tests ran as expected!"
+fi
