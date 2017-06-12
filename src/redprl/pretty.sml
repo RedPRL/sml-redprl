RedPRL/sml-redprl

commit 9b803eefbbb46d58f6612996867ec68e920f5f7a

Rename ID types to PATH types. (#193)

src/redprl/pretty.sml
@@ -54,7 +54,7 @@ struct
       POLY (LOOP p) $ [] =>
         SOME o atom @@ "loop[" ^ paramToString p ^ "]"
     | (* A @ p *)
-      POLY (ID_AP p) $ [_ \ a] =>
+      POLY (PATH_AP p) $ [_ \ a] =>
         SOME @@ infix' (Left, 5, "@") (unparse a, atom (paramToString p))
     | _ => NONE
 

src/redprl/machine.sml
@@ -163,11 +163,11 @@ struct
            @@ (O.MONO O.S1_ELIM `$ [([],[x]) \ S.% cx, ([],[]) \ S.HOLE, ([],[]) \ S.% b, ([u],[]) \ S.% l], m)
            <: env
 
-     | O.MONO O.ID_TY `$ _ <: _ => S.VAL
-     | O.MONO O.ID_ABS `$ _ <: _ => S.VAL
-     | O.POLY (O.ID_AP r) `$ [_ \ m] <: env =>
+     | O.MONO O.PATH_TY `$ _ <: _ => S.VAL
+     | O.MONO O.PATH_ABS `$ _ <: _ => S.VAL
+     | O.POLY (O.PATH_AP r) `$ [_ \ m] <: env =>
          S.CUT
-           @@ (O.POLY (O.ID_AP r) `$ [([],[]) \ S.HOLE], m)
+           @@ (O.POLY (O.PATH_AP r) `$ [([],[]) \ S.HOLE], m)
            <: env
 
      | O.MONO O.AX `$ _ <: _ => S.VAL
@@ -297,13 +297,13 @@ struct
            S.STEP @@ pair <: env
          end
 
-     | O.POLY (O.HCOM (O.TAG_ID, exts, dir)) `$ (([u],_) \ a) :: (_ \ p0) :: (_ \ p1) :: (_ \ cap) :: tubes <: env =>
+     | O.POLY (O.HCOM (O.TAG_PATH, exts, dir)) `$ (([u],_) \ a) :: (_ \ p0) :: (_ \ p1) :: (_ \ cap) :: tubes <: env =>
          let
-           fun ap m = Syn.into @@ Syn.ID_AP (m, P.ret u)
+           fun ap m = Syn.into @@ Syn.PATH_AP (m, P.ret u)
            val w = Sym.named "w"
            val tubes' = List.map (mapBind ap) tubes @ [([w],[]) \ p0, ([w],[]) \ p1]
            val hcom = O.POLY (O.HCOM (O.TAG_NONE, exts @ [P.ret u], dir)) $$ (([],[]) \ a) :: (([],[]) \ ap cap) :: tubes'
-           val path = Syn.into @@ Syn.ID_ABS (u, raise Match)
+           val path = Syn.into @@ Syn.PATH_ABS (u, raise Match)
          in
            S.STEP @@ path <: env
          end
@@ -339,9 +339,9 @@ struct
            S.STEP @@ pair <: env
          end
 
-     | O.POLY (O.COE (O.TAG_ID, dir)) `$ [([u,v],_) \ auv, p0, p1, _ \ m] <: env =>
+     | O.POLY (O.COE (O.TAG_PATH, dir)) `$ [([u,v],_) \ auv, p0, p1, _ \ m] <: env =>
          let
-           val com = Syn.heteroCom ([P.ret v], dir) ((u, auv), Syn.into @@ Syn.ID_AP (m, P.ret u), [p0, p1])
+           val com = Syn.heteroCom ([P.ret v], dir) ((u, auv), Syn.into @@ Syn.PATH_AP (m, P.ret u), [p0, p1])
          in
            S.STEP @@ com <: env
          end
@@ -398,7 +398,7 @@ struct
          end
 
 
-     | (O.POLY (O.ID_AP r) `$ [_ \ S.HOLE], _ \ O.MONO O.ID_ABS `$ [([u],_) \ m] <: env) =>
+     | (O.POLY (O.PATH_AP r) `$ [_ \ S.HOLE], _ \ O.MONO O.PATH_ABS `$ [([u],_) \ m] <: env) =>
          m <: Cl.insertSym env u r
 
      | (O.POLY (O.HCOM (O.TAG_NONE, exts, dir)) `$ ((_ \ S.HOLE) :: args), _ \ O.MONO O.BOOL `$ _ <: env) =>
@@ -436,10 +436,10 @@ struct
            hcom $$ a :: b :: args <: env
          end
 
-     | (O.POLY (O.HCOM (O.TAG_ID, exts, dir)) `$ ((_ \ S.HOLE) :: args), _ \ O.MONO O.ID_TY `$ [a, p0, p1] <: env) =>
+     | (O.POLY (O.HCOM (O.TAG_PATH, exts, dir)) `$ ((_ \ S.HOLE) :: args), _ \ O.MONO O.PATH_TY `$ [a, p0, p1] <: env) =>
          let
            val args = List.map (mapBind (fn S.% cl => Cl.force cl | _ => raise Match)) args
-           val hcom = O.POLY @@ O.HCOM (O.TAG_ID, exts, dir)
+           val hcom = O.POLY @@ O.HCOM (O.TAG_PATH, exts, dir)
          in
            hcom $$ a :: p0 :: p1 :: args <: env
          end
@@ -459,8 +459,8 @@ struct
      | (O.POLY (O.COE (O.TAG_NONE, dir)) `$ [_ \ S.HOLE, _\ S.% cl] , ([u],_) \ O.MONO O.DPROD `$ [_\ a, (_,[x]) \ bx] <: env) =>
          O.POLY (O.COE (O.TAG_DPROD, dir)) $$ [([u], []) \ a, ([u],[x]) \ bx, ([],[]) \ Cl.force cl] <: env
 
-     | (O.POLY (O.COE (O.TAG_NONE, dir)) `$ [_ \ S.HOLE, _\ S.% cl] , ([u],_) \ O.MONO O.ID_TY `$ [([v],_) \ a, _ \ p0, _ \ p1] <: env) =>
-         O.POLY (O.COE (O.TAG_ID, dir)) $$ [([u,v], []) \ a, ([u],[]) \ p0, ([u],[]) \ p1, ([],[]) \ Cl.force cl] <: env
+     | (O.POLY (O.COE (O.TAG_NONE, dir)) `$ [_ \ S.HOLE, _\ S.% cl] , ([u],_) \ O.MONO O.PATH_TY `$ [([v],_) \ a, _ \ p0, _ \ p1] <: env) =>
+         O.POLY (O.COE (O.TAG_PATH, dir)) $$ [([u,v], []) \ a, ([u],[]) \ p0, ([u],[]) \ p1, ([],[]) \ Cl.force cl] <: env
 
      | _ => raise InvalidCut
 end

src/redprl/operator.sml
@@ -100,7 +100,7 @@ struct
    | S_BOOL | S_IF (* strict booleans *)
    | S1 | BASE | S1_ELIM
    | AX
-   | ID_TY | ID_ABS
+   | PATH_TY | PATH_ABS
 
    (* primitive tacticals and multitacticals *)
    | MTAC_SEQ of psort list | MTAC_ORELSE | MTAC_REC
@@ -141,7 +141,7 @@ struct
    | TAG_S1
    | TAG_DFUN
    | TAG_DPROD
-   | TAG_ID
+   | TAG_PATH
 
   type psort = RedPrlArity.Vl.PS.t
   type 'a extents = 'a P.term list
@@ -153,7 +153,7 @@ struct
    | COE of type_tag * 'a dir
    | CUST of 'a * ('a P.term * psort option) list * RedPrlArity.t option
    | RULE_LEMMA of 'a * ('a P.term * psort option) list * RedPrlArity.t option
-   | ID_AP of 'a P.term
+   | PATH_AP of 'a P.term
    | HYP_REF of 'a
    | RULE_HYP of 'a * sort
    | RULE_ELIM of 'a * sort
@@ -205,8 +205,8 @@ struct
      | BASE => [] ->> EXP
      | S1_ELIM => [[] * [EXP] <> EXP, [] * [] <> EXP, [] * [] <> EXP, [DIM] * [] <> EXP] ->> EXP
      | AX => [] ->> TRIV
-     | ID_TY => [[DIM] * [] <> EXP, [] * [] <> EXP, [] * [] <> EXP] ->> EXP
-     | ID_ABS => [[DIM] * [] <> EXP] ->> EXP
+     | PATH_TY => [[DIM] * [] <> EXP, [] * [] <> EXP, [] * [] <> EXP] ->> EXP
+     | PATH_ABS => [[DIM] * [] <> EXP] ->> EXP
      | MTAC_SEQ psorts => [[] * [] <> MTAC, psorts * [] <> MTAC] ->> MTAC
      | MTAC_ORELSE => [[] * [] <> MTAC, [] * [] <> MTAC] ->> MTAC
      | MTAC_REC => [[] * [MTAC] <> MTAC] ->> MTAC
@@ -252,7 +252,7 @@ struct
        | TAG_S1 => []
        | TAG_DFUN => [[] * [] <> EXP, [] * [EXP] <> EXP]
        | TAG_DPROD => [[] * [] <> EXP, [] * [EXP] <> EXP]
-       | TAG_ID => [[DIM] * [] <> EXP, [] * [] <> EXP, [] * [] <> EXP]
+       | TAG_PATH => [[DIM] * [] <> EXP, [] * [] <> EXP, [] * [] <> EXP]
 
     fun arityHcom (tag, extents, dir) =
       let
@@ -282,7 +282,7 @@ struct
        | COE coe => arityCoe coe
        | CUST (_, _, ar) => Option.valOf ar
        | RULE_LEMMA (_, _, ar) => (#1 (Option.valOf ar), TAC)
-       | ID_AP r => [[] * [] <> EXP] ->> EXP
+       | PATH_AP r => [[] * [] <> EXP] ->> EXP
        | HYP_REF a => [] ->> EXP
        | RULE_HYP _ => [] ->> TAC
        | RULE_ELIM _ => [] ->> TAC
@@ -321,7 +321,7 @@ struct
        | COE (_, dir) => spanSupport dir
        | CUST (opid, ps, _) => (opid, OPID) :: paramsSupport ps
        | RULE_LEMMA (opid, ps, _) => (opid, OPID) :: paramsSupport ps
-       | ID_AP r => dimSupport r
+       | PATH_AP r => dimSupport r
        | HYP_REF a => [(a, HYP EXP)]
        | RULE_HYP (a, tau) => [(a, HYP tau)]
        | RULE_ELIM (a, tau) => [(a, HYP tau)]
@@ -400,8 +400,8 @@ struct
      | BASE => "base"
      | S1_ELIM => "s1-elim"
      | AX => "ax"
-     | ID_TY => "paths"
-     | ID_ABS => "abs"
+     | PATH_TY => "paths"
+     | PATH_ABS => "abs"
      | MTAC_SEQ _ => "seq"
      | MTAC_ORELSE => "orelse"
      | MTAC_REC => "rec"
@@ -448,7 +448,7 @@ struct
        | TAG_S1 => "/S1"
        | TAG_DFUN => "/dfun"
        | TAG_DPROD => "/dprod"
-       | TAG_ID => "/id"
+       | TAG_PATH => "/path"
   in
     fun toStringPoly f =
       fn LOOP r => "loop[" ^ P.toString f r ^ "]"
@@ -470,7 +470,7 @@ struct
            f opid ^ "{" ^ paramsToString f ps ^ "}"
        | RULE_LEMMA (opid, ps, ar) =>
            "lemma{" ^ f opid ^ "}{" ^ paramsToString f ps ^ "}"
-       | ID_AP r => "idap{" ^ P.toString f r ^ "}"
+       | PATH_AP r => "pathap{" ^ P.toString f r ^ "}"
        | HYP_REF a => "@" ^ f a
        | RULE_HYP (a, _) => "hyp{" ^ f a ^ "}"
        | RULE_ELIM (a, _) => "elim{" ^ f a ^ "}"
@@ -509,7 +509,7 @@ struct
        | COE (tag, dir) => COE (tag, mapSpan f dir)
        | CUST (opid, ps, ar) => CUST (mapSym f opid, mapParams f ps, ar)
        | RULE_LEMMA (opid, ps, ar) => RULE_LEMMA (mapSym f opid, mapParams f ps, ar)
-       | ID_AP r => ID_AP (P.bind f r)
+       | PATH_AP r => PATH_AP (P.bind f r)
        | HYP_REF a => HYP_REF (mapSym f a)
        | RULE_HYP (a, tau) => RULE_HYP (mapSym f a, tau)
        | RULE_ELIM (a, tau) => RULE_ELIM (mapSym f a, tau)

src/redprl/redprl.grm
@@ -290,7 +290,7 @@ operator
   | RULE_LEMMA OPNAME customOpParams (O.POLY (O.RULE_LEMMA (OPNAME, customOpParams, NONE)))
   | IF (O.MONO O.IF)
   | S_IF (O.MONO O.S_IF)
-  | PATHS (O.MONO O.ID_TY)
+  | PATHS (O.MONO O.PATH_TY)
   | FST (O.MONO O.FST)
   | SND (O.MONO O.SND)
 
@@ -323,8 +323,8 @@ atomicRawTerm
   | S1 (Ast.$$ (O.MONO O.S1, []))
   | BASE (Ast.$$ (O.MONO O.BASE, []))
   | LOOP LBRACKET param RBRACKET (Ast.$$ (O.POLY (O.LOOP param), []))
-  | LANGLE boundVar RANGLE term (Ast.$$ (O.MONO O.ID_ABS, [\ (([boundVar],[]), term)]))
-  | atomicTerm AT_SIGN param %prec AT_SIGN (Ast.$$ (O.POLY (O.ID_AP param), [\ (([],[]), atomicTerm)]))
+  | LANGLE boundVar RANGLE term (Ast.$$ (O.MONO O.PATH_ABS, [\ (([boundVar],[]), term)]))
+  | atomicTerm AT_SIGN param %prec AT_SIGN (Ast.$$ (O.POLY (O.PATH_AP param), [\ (([],[]), atomicTerm)]))
 
 atomicTerm : atomicRawTerm (annotate (Pos.pos (atomicRawTerm1left fileName) (atomicRawTerm1right fileName)) atomicRawTerm)
 

src/redprl/refiner.fun
@@ -609,8 +609,8 @@ struct
       let
         val _ = RedPrlLog.trace "Path.EqType"
         val H >> CJ.EQ_TYPE (ty0, ty1) = jdg
-        val Syn.ID_TY ((u, a0u), m0, n0) = Syn.out ty0
-        val Syn.ID_TY ((v, a1v), m1, n1) = Syn.out ty1
+        val Syn.PATH_TY ((u, a0u), m0, n0) = Syn.out ty0
+        val Syn.PATH_TY ((v, a1v), m1, n1) = Syn.out ty1
 
         val a1u = substSymbol (P.ret u, v) a1v
 
@@ -629,7 +629,7 @@ struct
       let
         val _ = RedPrlLog.trace "Path.True"
         val H >> CJ.TRUE ty = jdg
-        val Syn.ID_TY ((u, a), p0, p1) = Syn.out ty
+        val Syn.PATH_TY ((u, a), p0, p1) = Syn.out ty
         val a0 = substSymbol (P.APP P.DIM0, u) a
         val a1 = substSymbol (P.APP P.DIM1, u) a
 
@@ -643,7 +643,7 @@ struct
         val (cohGoal1, _) = makeGoal @@ ([],[]) || H >> CJ.EQ ((m1, p1), a1)
 
         val psi = T.empty >: mainGoal >: cohGoal0 >: cohGoal1
-        val abstr = Syn.into @@ Syn.ID_ABS (v, mhole [(P.ret v, P.DIM)] [])
+        val abstr = Syn.into @@ Syn.PATH_ABS (v, mhole [(P.ret v, P.DIM)] [])
       in
         psi #> abstr
       end
@@ -652,9 +652,9 @@ struct
       let
         val _ = RedPrlLog.trace "Path.Eq"
         val H >> CJ.EQ ((abs0, abs1), ty) = jdg
-        val Syn.ID_TY ((u, au), p0, p1) = Syn.out ty
-        val Syn.ID_ABS (v, m0v) = Syn.out abs0
-        val Syn.ID_ABS (w, m1w) = Syn.out abs1
+        val Syn.PATH_TY ((u, au), p0, p1) = Syn.out ty
+        val Syn.PATH_ABS (v, m0v) = Syn.out abs0
+        val Syn.PATH_ABS (w, m1w) = Syn.out abs1
 
         val z = alpha 0
         val az = substSymbol (P.ret z, u) au
@@ -678,12 +678,12 @@ struct
       let
         val _ = RedPrlLog.trace "Path.ApEq"
         val H >> CJ.EQ ((ap0, ap1), ty) = jdg
-        val Syn.ID_AP (m0, r0) = Syn.out ap0
-        val Syn.ID_AP (m1, r1) = Syn.out ap1
+        val Syn.PATH_AP (m0, r0) = Syn.out ap0
+        val Syn.PATH_AP (m1, r1) = Syn.out ap1
         val () = assertParamEq "Path.ApEq" (r0, r1)
         val (goalSynth, holeSynth) = makeGoal @@ ([],[]) || H >> CJ.SYNTH m0
         val (goalMem, _) = makeGoal @@ ([],[]) || H >> CJ.EQ ((m0, m1), holeSynth [] [])
-        val (goalLine, holeLine) = makeGoal @@ ([],[]) || MATCH (O.MONO O.ID_TY, 0, holeSynth [] [])
+        val (goalLine, holeLine) = makeGoal @@ ([],[]) || MATCH (O.MONO O.PATH_TY, 0, holeSynth [] [])
         val (goalTy, _) = makeGoal @@ ([],[]) || H >> CJ.EQ_TYPE (ty, holeLine [(r0, P.DIM)] [])
       in
         T.empty >: goalSynth >: goalMem >: goalLine >: goalTy
@@ -694,12 +694,12 @@ struct
       let
         val _ = RedPrlLog.trace "Path.ApComputeConst"
         val H >> CJ.EQ ((ap, p), a) = jdg
-        val Syn.ID_AP (m, P.APP r) = Syn.out ap
+        val Syn.PATH_AP (m, P.APP r) = Syn.out ap
         val (goalSynth, holeSynth) = makeGoal @@ ([],[]) || H >> CJ.SYNTH m
 
         val dimAddr = case r of P.DIM0 => 1 | P.DIM1 => 2 | _ => raise Fail "impossible"
-        val (goalLine, holeLine) = makeGoal @@ ([],[]) || MATCH (O.MONO O.ID_TY, 0, holeSynth [] [])
-        val (goalEndpoint, holeEndpoint) = makeGoal @@ ([],[]) || MATCH (O.MONO O.ID_TY, dimAddr, holeSynth [] [])
+        val (goalLine, holeLine) = makeGoal @@ ([],[]) || MATCH (O.MONO O.PATH_TY, 0, holeSynth [] [])
+        val (goalEndpoint, holeEndpoint) = makeGoal @@ ([],[]) || MATCH (O.MONO O.PATH_TY, dimAddr, holeSynth [] [])
         val (goalTy, _) = makeGoal @@ ([],[]) || H >> CJ.EQ_TYPE (a, holeLine [(P.APP r, P.DIM)] [])
         val (goalEq, _) = makeGoal @@ ([],[]) || H >> CJ.EQ ((holeEndpoint [] [], p), a)
       in
@@ -711,9 +711,9 @@ struct
       let
         val _ = RedPrlLog.trace "Path.Eta"
         val H >> CJ.EQ ((m, n), pathTy) = jdg
-        val Syn.ID_TY ((u, a), p0, p1) = Syn.out pathTy
+        val Syn.PATH_TY ((u, a), p0, p1) = Syn.out pathTy
 
-        val m' = Syn.into @@ Syn.ID_ABS (u, Syn.into @@ Syn.ID_AP (m, P.ret u))
+        val m' = Syn.into @@ Syn.PATH_ABS (u, Syn.into @@ Syn.PATH_AP (m, P.ret u))
         val (goal1, _) = makeGoal @@ ([],[]) || H >> CJ.MEM (m, pathTy)
         val (goal2, _) = makeGoal @@ ([],[]) || H >> CJ.EQ ((m', n), pathTy)
       in
@@ -868,9 +868,9 @@ struct
       let
         val _ = RedPrlLog.trace "Synth.PathAp"
         val H >> CJ.SYNTH tm = jdg
-        val Syn.ID_AP (m, r) = Syn.out tm
+        val Syn.PATH_AP (m, r) = Syn.out tm
         val (goalPathTy, holePathTy) = makeGoal @@ ([],[]) || H >> CJ.SYNTH m
-        val (goalLine, holeLine) = makeGoal @@ ([],[]) || MATCH (O.MONO O.ID_TY, 0, holePathTy [] [])
+        val (goalLine, holeLine) = makeGoal @@ ([],[]) || MATCH (O.MONO O.PATH_TY, 0, holePathTy [] [])
         val psi = T.empty >: goalPathTy >: goalLine
       in
         T.empty >: goalPathTy >: goalLine
@@ -1376,7 +1376,7 @@ struct
         case Syn.out ty of
            Syn.DFUN _ => DFun.True
          | Syn.DPROD _ => DProd.True
-         | Syn.ID_TY _ => Path.True
+         | Syn.PATH_TY _ => Path.True
          | _ => raise E.error [E.% "Could not find introduction rule for", E.! ty]
 
       fun StepEqTypeVal (ty1, ty2) =
@@ -1385,7 +1385,7 @@ struct
          | (Syn.S_BOOL, Syn.S_BOOL) => StrictBool.EqType
          | (Syn.DFUN _, Syn.DFUN _) => DFun.EqType
          | (Syn.DPROD _, Syn.DPROD _) => DProd.EqType
-         | (Syn.ID_TY _, Syn.ID_TY _) => Path.EqType
+         | (Syn.PATH_TY _, Syn.PATH_TY _) => Path.EqType
          | (Syn.S1, Syn.S1) => S1.EqType
          | _ => raise E.error [E.% "Could not find type equality rule for", E.! ty1, E.% "and", E.! ty2]
 
@@ -1406,7 +1406,7 @@ struct
          | (Syn.BASE, Syn.BASE, Syn.S1) => S1.EqBase
          | (Syn.LOOP _, Syn.LOOP _, Syn.S1) => S1.EqLoop
          | (Syn.LAM _, Syn.LAM _, _) => DFun.Eq
-         | (Syn.ID_ABS _, Syn.ID_ABS _, _) => Path.Eq
+         | (Syn.PATH_ABS _, Syn.PATH_ABS _, _) => Path.Eq
          | (Syn.PAIR _, Syn.PAIR _, _) => DProd.Eq
          | _ => raise E.error [E.% "Could not find value equality rule for", E.! m, E.% "and", E.! n, E.% "at type", E.! ty]
 
@@ -1426,16 +1426,16 @@ struct
          | (Syn.AP _, Syn.AP _, _) => DFun.ApEq
          | (Syn.FST _, Syn.FST _, _) => DProd.FstEq
          | (Syn.SND _, Syn.SND _, _) => DProd.SndEq
-         | (Syn.ID_AP (_, P.VAR _), Syn.ID_AP (_, P.VAR _), _) => Path.ApEq
-         | (Syn.ID_AP (_, P.APP _), _, _) => Path.ApComputeConst
-         | (_, Syn.ID_AP (_, P.APP _), _) => Equality.Symmetry
+         | (Syn.PATH_AP (_, P.VAR _), Syn.PATH_AP (_, P.VAR _), _) => Path.ApEq
+         | (Syn.PATH_AP (_, P.APP _), _, _) => Path.ApComputeConst
+         | (_, Syn.PATH_AP (_, P.APP _), _) => Equality.Symmetry
          | _ => raise E.error [E.% "Could not find neutral equality rule for", E.! m, E.% "and", E.! n, E.% "at type", E.! ty]
 
       fun StepEqEta ty =
         case Syn.out ty of
            Syn.DPROD _ => DProd.Eta
          | Syn.DFUN _ => DFun.Eta
-         | Syn.ID_TY _ => Path.Eta
+         | Syn.PATH_TY _ => Path.Eta
          | _ => raise E.error [E.% "Could not find eta expansion rule for type", E.! ty]
 
       fun StepEqCanonicity sign ((m, n), ty) =
@@ -1460,7 +1460,7 @@ struct
          | Syn.AP _ => Synth.Ap
          | Syn.S1_ELIM _ => Synth.S1Elim
          | Syn.IF _ => Synth.If
-         | Syn.ID_AP _ => Synth.PathAp
+         | Syn.PATH_AP _ => Synth.PathAp
          | Syn.FST _ => Synth.Fst
          | Syn.SND _ => Synth.Snd
          | _ => raise E.error [E.% "Could not find suitable type synthesis rule for", E.! m]

src/redprl/syntax.sml
@@ -16,7 +16,7 @@ struct
    | S1 | BASE | LOOP of param | S1_ELIM of (variable * 'a) * 'a * ('a * (symbol * 'a))
    | DFUN of 'a * variable * 'a | LAM of variable * 'a | AP of 'a * 'a
    | DPROD of 'a * variable * 'a | PAIR of 'a * 'a | FST of 'a | SND of 'a
-   | ID_TY of (symbol * 'a) * 'a * 'a | ID_ABS of symbol * 'a | ID_AP of 'a * param
+   | PATH_TY of (symbol * 'a) * 'a * 'a | PATH_ABS of symbol * 'a | PATH_AP of 'a * param
    | HCOM of param list (* extents *) * dir * 'a (* type *) * 'a (* cap *) * (symbol * 'a) list (* tubes *)
    | CUST
    | META
@@ -46,9 +46,9 @@ struct
        | PAIR (m, n) => O.MONO O.PAIR $$ [([],[]) \ m, ([],[]) \ n]
        | FST m => O.MONO O.FST $$ [([],[]) \ m]
        | SND m => O.MONO O.SND $$ [([],[]) \ m]
-       | ID_TY ((u, a), m, n) => O.MONO O.ID_TY $$ [([u],[]) \ a, ([],[]) \ m, ([],[]) \ n]
-       | ID_ABS (u, m) => O.MONO O.ID_ABS $$ [([u],[]) \ m]
-       | ID_AP (m, r) => O.POLY (O.ID_AP r) $$ [([],[]) \ m]
+       | PATH_TY ((u, a), m, n) => O.MONO O.PATH_TY $$ [([u],[]) \ a, ([],[]) \ m, ([],[]) \ n]
+       | PATH_ABS (u, m) => O.MONO O.PATH_ABS $$ [([u],[]) \ m]
+       | PATH_AP (m, r) => O.POLY (O.PATH_AP r) $$ [([],[]) \ m]
        | HCOM (exts, dir, ty, cap, tubes) => O.POLY (O.HCOM (O.TAG_NONE, exts, dir)) $$ (([],[]) \ ty) :: (([],[]) \ cap) :: List.map (fn (d, tube) => ([d], []) \ tube) tubes
        | CUST => raise Fail "CUST"
        | META => raise Fail "META"
@@ -74,9 +74,9 @@ struct
        | O.MONO O.PAIR $ [_ \ m, _ \ n] => PAIR (m, n)
        | O.MONO O.FST $ [_ \ m] => FST m
        | O.MONO O.SND $ [_ \ m] => SND m
-       | O.MONO O.ID_TY $ [([u],_) \ a, _ \ m, _ \ n] => ID_TY ((u, a), m, n)
-       | O.MONO O.ID_ABS $ [([u],_) \ m] => ID_ABS (u, m)
-       | O.POLY (O.ID_AP r) $ [_ \ m] => ID_AP (m, r)
+       | O.MONO O.PATH_TY $ [([u],_) \ a, _ \ m, _ \ n] => PATH_TY ((u, a), m, n)
+       | O.MONO O.PATH_ABS $ [([u],_) \ m] => PATH_ABS (u, m)
+       | O.POLY (O.PATH_AP r) $ [_ \ m] => PATH_AP (m, r)
        | O.POLY (O.HCOM (tag, exts, dir)) $ args =>
          let
            val (ty, args) =
@@ -86,7 +86,7 @@ struct
               | (O.TAG_S1, args) => (O.MONO O.S1 $$ [], args)
               | (O.TAG_DFUN, (A :: xB :: args)) => (O.MONO O.DFUN $$ [A, xB], args)
               | (O.TAG_DPROD, (A :: xB :: args)) => (O.MONO O.DPROD $$ [A, xB], args)
-              | (O.TAG_ID, (uA :: a0 :: a1 :: args)) => (O.MONO O.ID_TY $$ [uA, a0, a1], args)
+              | (O.TAG_PATH, (uA :: a0 :: a1 :: args)) => (O.MONO O.PATH_TY $$ [uA, a0, a1], args)
               | _ => raise Fail "Syntax.out hcom: Malformed tag"
            val (_ \ cap) :: args = args
            fun goTube (([d], _) \ tube) = (d, tube)

commit 37070de3e087b9b896c0792378aa8a3cda88feee

Fix a bug introduced by 9b803ee. See #193 and #194.

src/redprl/pretty.sml
@@ -48,7 +48,7 @@ struct
       MONO PAIR $ [_ \ m, _ \ n] =>
         SOME o atom @@ "<" ^ toString m ^ ", " ^ toString n ^ ">"
     | (* <x> A *)
-      MONO ID_ABS $ [(([x], []) \ a)] =>
+      MONO PATH_ABS $ [(([x], []) \ a)] =>
         SOME o atom @@ "<" ^ ShowVar.toString x  ^ "> " ^ toString a
     | (* loop[p] *)
       POLY (LOOP p) $ [] =>