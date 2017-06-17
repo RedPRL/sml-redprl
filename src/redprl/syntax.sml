RedPRL/sml-redprl

commit 4926bd3a0dc6c903dd44720ee18e7bd7691fd26e

add helper for heteroCom

src/redprl/syntax.sml
@@ -15,7 +15,7 @@ struct
 
   local
     open Tm
-    structure O = RedPrlOpData
+    structure O = RedPrlOpData and P = RedPrlParameterTerm
     infix $ $$ \
   in
     val into =
@@ -45,5 +45,16 @@ struct
        | O.MONO O.DFUN $ [_ \ a, (_,[x]) \ bx] => DFUN (a, x, bx)
        | O.MONO O.LAM $ [(_,[x]) \ mx] => LAM (x, mx)
        | _ => raise Match
+
+    fun heteroCom (exts, dir) ((u, a), cap, tube) =
+      let
+        val (r, r') = dir
+        fun coe v m = O.POLY (O.COE (O.TAG_NONE, dir)) $$ [([u],[]) \ a, ([],[]) \ m]
+        val ty = ([],[]) \ substSymbol (r', u) a
+        val cap' = ([],[]) \ coe r cap
+        val tube' = List.map (fn ([v],_) \ n => ([v],[]) \ coe (P.ret v) n | _ => raise Fail "malformed tube") tube
+      in
+        O.POLY (O.HCOM (O.TAG_NONE, exts, dir)) $$ ty :: cap' :: tube'
+      end
   end
 end

commit c529e8c76ed057956409f533596ca5702e91496a

Fix a bug in heteroCom introduced by 4926bd3.

src/redprl/syntax.sml
@@ -134,7 +134,7 @@ struct
     fun heteroCom (eqs, dir) ((u, a), cap, tube) =
       let
         val (r, r') = dir
-        fun coe v m = O.POLY (O.COE (O.TAG_NONE, dir)) $$ [([u],[]) \ a, ([],[]) \ m]
+        fun coe v m = O.POLY (O.COE (O.TAG_NONE, (v, r'))) $$ [([u],[]) \ a, ([],[]) \ m]
         val ty = ([],[]) \ substSymbol (r', u) a
         val cap' = ([],[]) \ coe r cap
         val tube' = List.map (fn ([v],_) \ n => ([v],[]) \ coe (P.ret v) n | _ => raise Fail "malformed tube") tube