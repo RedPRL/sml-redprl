7a840901792a95d7e0e81f9dd77a1687e1880ca9
Jon Sterling <jon@jonmsterling.com>
2017-07-25 10:19:13 -0400

    break up test script into version that doesn't build first

---
+++ script/test-no-build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+REDPRL=./bin/redprl
+PROBLEM=0
+
+# Ensure that the examples file and all suceeding tests succeed
+for f in test/examples.prl test/success/*.prl ; do
+    if ! $REDPRL $f >$f.log 2>&1 ; then
+        PROBLEM=1
+        echo "Test $f should succeed but failed!"
+    fi
+done
+
+
+# Ensure that failing tests fail
+for f in test/failure/*.prl ; do
+    if $REDPRL $f >$f.log 2>&1 ; then
+        PROBLEM=1
+        echo "Test $f should fail but succeeded!"
+    fi
+done
+
+if [ $PROBLEM -eq 0 ] ; then
+    echo "All tests ran as expected!"
+fi
+
+exit $PROBLEM
