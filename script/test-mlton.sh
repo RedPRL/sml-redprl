6dc3e46fab5b29b4a18b745a807ccd2a22309c03
Jonathan Sterling <jonsterling@users.noreply.github.com>
2017-10-07 09:53:07 -0400

    get MLton running again using a suggestion from @MatthewFluet (#399)
    https://github.com/MLton/mlton/issues/196#issuecomment-334900882
    
    issue #394

--- script/test-mlton.sh
+++ script/test-mlton.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Building RedPRL with MLton..."
-./script/mlton-tc.sh >build.log 2>&1 || { echo "build failed!"; cat build.log; exit 1; }
+./script/mlton.sh >build.log 2>&1 || { echo "build failed!"; cat build.log; exit 1; }
 echo "done!"
 
 # exec ./script/test-no-build.sh
