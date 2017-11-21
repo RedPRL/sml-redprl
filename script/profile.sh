47848a0d5c79a60a0a029f7cfbe3d1697587a947
Matthew Fluet <matthew.fluet@gmail.com>
2017-11-21 12:04:42 -0500

    Update mlton compile options
    Closes RedPRL/sml-redprl#437
    
    MLton's `polyvariance` optimization duplicates small higher-order
    functions.
    
    One (unresolved, though usually not significant) issue is that
    polyvariance can duplicate code local to a function, even if it
    doesn't depend on the higher-orderness (see
    http://www.mlton.org/pipermail/mlton-devel/2002-January/021211.html).
    This seems consistent with the discussion at MLton/mlton#196.

--- script/profile.sh
+++ script/profile.sh
@@ -6,4 +6,4 @@ mlyacc src/redprl/redprl.grm
 mllex src/redprl/redprl.lex
 
 mkdir -p bin
-mlton -profile time -mlb-path-var "LIBS $LIBS" -output bin/redprl-profile src/frontend.mlb
+mlton -profile time -polyvariance false -mlb-path-var "LIBS $LIBS" -output bin/redprl-profile src/frontend.mlb
