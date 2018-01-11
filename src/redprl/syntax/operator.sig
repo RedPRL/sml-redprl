RedPRL/sml-redprl

commit 3df6aa8888c282112c7df9508ff94cfded2bbff9

Remove member equality judgments. (#524)
* Remove member equality judgments.
* Remove the rewrite-hyp tactic.
* Make the grammar of reduce more flexible.
* Fix a bug introduced in #528.
* Update examples.

src/redprl/syntax/operator.sig
@@ -68,7 +68,6 @@ struct
 
    | KCONST of kind
 
-   | JDG_EQ
    | JDG_TRUE
    | JDG_EQ_TYPE
    | JDG_SUB_TYPE

src/redprl/syntax/operator.sml
@@ -108,7 +108,6 @@ struct
      | KCONST _ => [] ->> KND
 
 
-     | JDG_EQ => [[] |: EXP, [] |: EXP, [] |: EXP] ->> JDG
      | JDG_TRUE => [[] |: EXP] ->> JDG
      | JDG_EQ_TYPE => [[] |: KND, [] |: EXP, [] |: EXP] ->> JDG
      | JDG_SUB_TYPE => [[] |: EXP, [] |: EXP] ->> JDG
@@ -140,7 +139,6 @@ struct
      | RULE_PRIM _ => [] ->> TAC
      | TAC_ELIM => [[] |: ANY] ->> TAC
      | TAC_REWRITE => [[] |: SEL, [] |: ACC, [] |: EXP] ->> TAC
-     | TAC_REWRITE_HYP => [[] |: SEL, [] |: ANY] ->> TAC
 
      | DEV_FUN_INTRO pats => [List.concat (List.map devPatternValence pats) |: TAC] ->> TAC
      | DEV_RECORD_INTRO lbls => List.map (fn _ => [] |: TAC) lbls ->> TAC
@@ -287,7 +285,6 @@ struct
      | RULE_PRIM name => "refine{" ^ name ^ "}"
      | TAC_ELIM => "elim"
      | TAC_REWRITE => "rewrite"
-     | TAC_REWRITE_HYP => "rewrite-hyp"
 
      | DEV_PATH_INTRO n => "path-intro{" ^ Int.toString n ^ "}"
      | DEV_FUN_INTRO pats => "fun-intro"
@@ -316,7 +313,6 @@ struct
      | SEL_CONCL => "select-goal"
      | PAT_META _ => "pat-meta"
 
-     | JDG_EQ => "eq"
      | JDG_TRUE => "true"
      | JDG_EQ_TYPE => "eq-type"
      | JDG_SUB_TYPE => "sub-type"

emacs/redprl.el
@@ -137,7 +137,7 @@
 (defconst redprl-tactic-keywords
   '("auto" "auto-step" "case" "concl" "cut-lemma" "elim" "else" "exact" "fresh" "goal"
     "hyp" "id" "lemma" "let" "claim" "match" "of" "print" "progress"
-    "query" "rec" "reduce" "refine" "repeat" "rewrite" "rewrite-hyp" "symmetry"
+    "query" "rec" "reduce" "refine" "repeat" "rewrite" "symmetry"
     "then" "unfold" "use" "with")
   "RedPRL's tactic keywords.")
 

example/pi1s1.prl
@@ -50,7 +50,7 @@ Thm IntSuccIsEquiv : [
 ] by [
   lam i =>
     claim eq : [(= int ($ IntSucc ($ IntPred i)) i)] by [use IntSuccIntPred [`i]];
-    fresh eq' -> elim eq; unfold IntSucc IntPred in eq'; reduce in eq';
+    unfold IntSucc IntPred in eq; reduce at left in eq;
     {
       {use IntPred [`i], abs _ => `i}
     , lam {i',p'} =>
@@ -64,8 +64,8 @@ Thm IntSuccIsEquiv : [
 
         (abs x =>
           {`($ IntPred i), abs y => `(hcom 1~>y int i [x=0 [y] (@ p' y)] [x=1 [_] i])});
-        
-        auto; fresh eq1' -> elim eq1; unfold IntPred in eq1'; reduce in eq1'; auto
+
+        auto; unfold IntPred in eq1; reduce at left in eq1; auto
     }
 ].
 

example/semi-simplicial.prl
@@ -38,7 +38,7 @@ Thm Pick/succ/0/succ/1 : [
   lam n m =>
     claim aux0 : [($ Pick n m) in (U 0)] by [auto];
     claim aux1 : [($ Pick (succ n) m) in (U 0)] by [auto];
-    unfold Pick; reduce; auto
+    unfold Pick; reduce at left in aux0; reduce at left in aux1; auto
 ].
 
 Tac Replace(#z, #A) = [
@@ -82,7 +82,7 @@ Thm Pick/compose : [
         ]
       ]
     ]
-  ]
+  ]; auto
 ].
 
 Thm Pick/compose/zero/0 : [
@@ -114,7 +114,11 @@ Thm Pick/compose/tt/4/0/tt/5/0 : [
       ] by [auto];
     claim aux1 : [ ($ Pick (succ a) c) type ] by [auto];
     claim aux2 : [ ($ Pick a) in (-> nat (U 0)) ] by [auto];
-      refine eq/intro; unfold Pick/compose Pick; reduce; auto
+    unfold Pick/compose Pick;
+    reduce at type left right in aux0;
+    reduce at left right in aux1;
+    reduce at left in aux2;
+    auto
 ].
 
 Thm Pick/compose/tt/4/0/ff/5/0 : [
@@ -136,7 +140,10 @@ Thm Pick/compose/tt/4/0/ff/5/0 : [
       ] by [auto];
     claim aux1 : [ ($ Pick a c) type ] by [auto];
     claim aux2 : [ ($ Pick a) in (-> nat (U 0)) ] by [auto];
-    unfold Pick Pick/compose; reduce; auto
+    unfold Pick Pick/compose;
+    reduce at type left right in aux0;
+    reduce at left in aux2;
+    auto
 ].
 
 Thm Pick/compose/ff/4/0 : [
@@ -158,5 +165,8 @@ Thm Pick/compose/ff/4/0 : [
       ] by [auto];
     claim aux1 : [ ($ Pick a c) type ] by [auto];
     claim aux2 : [ ($ Pick a) in (-> nat (U 0)) ] by [auto];
-    unfold Pick Pick/compose; reduce; auto
+    unfold Pick Pick/compose;
+    reduce at type left right in aux0;
+    reduce at left in aux2;
+    auto
 ].

example/theorem-of-choice.prl
@@ -10,7 +10,7 @@ Thm Choice(#i:lvl) : [
   lam a b r f =>
     {lam x => let {y,_} = f [`x]; `y,
      lam x => let {_,z} = f [`x]; `z};
-    inversion; reduce; auto
+    fresh aux0 _ -> inversion; reduce at left in aux0; auto
 ].
 
 Print Choice.

src/redprl/redprl.grm
@@ -345,7 +345,7 @@ end
  | THEN | ELSE
  | REFINE
  | MTAC_REC | MTAC_PROGRESS | MTAC_REPEAT | MTAC_AUTO | MTAC_HOLE
- | TAC_ID | TAC_FAIL | TAC_AUTO_STEP | TAC_SYMMETRY | TAC_ELIM | TAC_REWRITE | TAC_REWRITE_HYP | TAC_REDUCE | TAC_UNFOLD
+ | TAC_ID | TAC_FAIL | TAC_AUTO_STEP | TAC_SYMMETRY | TAC_ELIM | TAC_REWRITE | TAC_REDUCE | TAC_UNFOLD
  | RULE_EXACT
  | TAC_INVERSION
  | MATCH
@@ -790,8 +790,8 @@ withKind
   | (Ast.$$ (O.KCONST K.top, []))
 
 rawJudgment
-  : term EQUALS term IN term (Ast.$$ (O.JDG_EQ, [\\ term1, \\ term2, \\ term3]))
-  | term IN term (Ast.$$ (O.JDG_EQ, [\\ term1, \\ term1, \\ term2]))
+  : term EQUALS term IN term (Ast.$$ (O.JDG_TRUE, [\\ (Ast.$$ (O.EQUALITY, [\\ term3, \\ term1, \\ term2]))]))
+  | term IN term (Ast.$$ (O.JDG_TRUE, [\\ (Ast.$$ (O.EQUALITY, [\\ term2, \\ term1, \\ term1]))]))
   | term TRUE (Ast.$$ (O.JDG_TRUE, [\\ term]))
   | term (Ast.$$ (O.JDG_TRUE, [\\ term]))
   | term EQUALS term TYPE withKind (Ast.$$ (O.JDG_EQ_TYPE, [\\ withKind, \\ term1, \\ term2]))
@@ -897,11 +897,11 @@ atomicRawTac
   | TAC_REWRITE term IN selector (Ast.$$ (O.TAC_REWRITE, [\\ selector, \\ (Ast.$$ (O.ACC_WHOLE, [])), \\ term]))
   | TAC_REWRITE term AT accessor IN selector (Ast.$$ (O.TAC_REWRITE, [\\ selector, \\ accessor, \\ term]))
   | TAC_REWRITE term IN selector AT accessor (Ast.$$ (O.TAC_REWRITE, [\\ selector, \\ accessor, \\ term]))
-  | TAC_REWRITE_HYP elimTargetAnySort (Ast.$$ (O.TAC_REWRITE_HYP, [Ast.\ ([], Ast.$$ (O.SEL_CONCL, [])), Ast.\ ([], elimTargetAnySort)]))
-  | TAC_REWRITE_HYP elimTargetAnySort IN selector (Ast.$$ (O.TAC_REWRITE_HYP, [Ast.\ ([], selector), Ast.\ ([], elimTargetAnySort)]))
   | TAC_REDUCE (Ast.$$ (O.TAC_REDUCE_ALL, []))
   | TAC_REDUCE IN selectors (Ast.$$ (O.TAC_REDUCE, [Ast.\ ([], Multi.makeVec O.SEL selectors)]))
+  | TAC_REDUCE AT accessors (Ast.$$ (O.TAC_REDUCE_PART, [\\ (Ast.$$ (O.SEL_CONCL, [])), \\ (Multi.makeVec O.ACC accessors)]))
   | TAC_REDUCE IN selector AT accessors (Ast.$$ (O.TAC_REDUCE_PART, [\\ selector, \\ (Multi.makeVec O.ACC accessors)]))
+  | TAC_REDUCE AT accessors IN selector (Ast.$$ (O.TAC_REDUCE_PART, [\\ selector, \\ (Multi.makeVec O.ACC accessors)]))
   | TAC_UNFOLD opnames (Ast.$$ (O.TAC_UNFOLD_ALL opnames, []))
   | TAC_UNFOLD opnames IN selectors (Ast.$$ (O.TAC_UNFOLD (opnames), [Ast.\ ([], Multi.makeVec O.SEL selectors)]))
 

src/redprl/redprl.lex
@@ -175,7 +175,6 @@ whitespace = [\ \t];
 "auto"             => (Tokens.MTAC_AUTO (posTuple (size yytext)));
 "elim"             => (Tokens.TAC_ELIM (posTuple (size yytext)));
 "rewrite"          => (Tokens.TAC_REWRITE (posTuple (size yytext)));
-"rewrite-hyp"      => (Tokens.TAC_REWRITE_HYP (posTuple (size yytext)));
 "reduce"           => (Tokens.TAC_REDUCE (posTuple (size yytext)));
 "unfold"           => (Tokens.TAC_UNFOLD (posTuple (size yytext)));
 "exact"            => (Tokens.RULE_EXACT (posTuple (size yytext)));

commit a7d018a473de2bc50ce0e396d445a57411c54bd6

Fix bugs introduced by #524.

src/redprl/syntax/operator.sig
@@ -87,7 +87,6 @@ struct
    | RULE_PRIM of string
    | TAC_ELIM
    | TAC_REWRITE
-   | TAC_REWRITE_HYP
    | TAC_REDUCE_ALL
    | TAC_REDUCE
    | TAC_REDUCE_PART

src/redprl/syntax/operator.sml
@@ -299,6 +299,12 @@ struct
 
      | SEL_HYP => "select-hyp"
      | SEL_CONCL => "select-goal"
+
+     | ACC_WHOLE => "accessor-whole"
+     | ACC_LEFT => "accessor-left"
+     | ACC_RIGHT => "accessor-right"
+     | ACC_TYPE => "accessor-type"
+
      | PAT_META _ => "pat-meta"
 
      | JDG_TRUE => "true"