2d4a91ac8b54edeae656c11d7703d098c5872a0d
favonia <favonia@gmail.com>
2018-02-12 00:15:13 -0600

    Profiling should use the same parameters as for building.

--- script/profile.sh
+++ script/profile.sh
@@ -6,4 +6,4 @@ mlyacc src/redprl/redprl.grm
 mllex src/redprl/redprl.lex
 
 mkdir -p bin
-mlton -profile time -polyvariance false -mlb-path-var "LIBS $LIBS" -output bin/redprl-profile src/frontend.mlb
+mlton -profile time -verbose 2 -polyvariance false -drop-pass deepFlatten -mlb-path-var "LIBS $LIBS" -output bin/redprl-profile src/frontend.mlb
