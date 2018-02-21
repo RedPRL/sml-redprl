96425a22baed8a90d6ace7f0d96822299b69d4e3
Jonathan Sterling <jon@jonmsterling.com>
2018-02-21 16:17:18 -0500

    set global highlight language to not be python, lol

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -65,6 +65,8 @@ release = u''
 # Usually you set "language" from the command line for these cases.
 language = None
 
+highlight_language = 'none'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
