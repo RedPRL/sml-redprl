87a3ac70b4ea1e52fc453d201d4d0e38b0751221
favonia <favonia@gmail.com>
2018-02-22 09:37:27 -0500

    Try another way to install python libs.

--- script/test-sphinx.sh
+++ script/test-sphinx.sh
@@ -5,7 +5,8 @@ set -o pipefail
 echo "Building the documentation of RedPRL with Sphinx..."
 cd doc;
 if [ -n "${TRAVIS}" ]; then
-  pip install sphinx-rtd-theme || exit 1;
+  pip install --user requests[security] --upgrade || exit 1;
+  pip install --user sphinx-rtd-theme || exit 1;
   make SPHINXOPTS="-n -W" html || exit 1;
 else
   make html || exit 1;
