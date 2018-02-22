ab7e7e18076b3be39348a05cf9fd249bcb747306
favonia <favonia@gmail.com>
2018-02-22 09:59:18 -0500

    Try to install all Python libs in the userland.

--- script/test-sphinx.sh
+++ script/test-sphinx.sh
@@ -5,8 +5,8 @@ set -o pipefail
 echo "Building the documentation of RedPRL with Sphinx..."
 cd doc;
 if [ -n "${TRAVIS}" ]; then
-  pip install --user requests[security] --upgrade || exit 1;
-  pip install --user sphinx-rtd-theme || exit 1;
+  pip install --user --upgrade requests[security] || exit 1;
+  pip install --user --upgrade Sphinx sphinx-rtd-theme || exit 1;
   make SPHINXOPTS="-n -W" html || exit 1;
 else
   make html || exit 1;
