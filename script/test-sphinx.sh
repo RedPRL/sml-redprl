da67dbbb0b811511a790492f607ca75e781791d9
favonia <favonia@gmail.com>
2018-02-22 09:33:13 -0500

    Give travis yet another chance.

--- script/test-sphinx.sh
+++ script/test-sphinx.sh
@@ -5,6 +5,7 @@ set -o pipefail
 echo "Building the documentation of RedPRL with Sphinx..."
 cd doc;
 if [ -n "${TRAVIS}" ]; then
+  pip install sphinx-rtd-theme || exit 1;
   make SPHINXOPTS="-n -W" html || exit 1;
 else
   make html || exit 1;
