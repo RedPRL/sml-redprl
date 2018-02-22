d250272f31d6b9bf0ee2cbf86d9d8c3804bea0e3
Jonathan Sterling <jon@jonmsterling.com>
2018-02-22 08:53:28 -0500

    configure travis-specific env vars in .travis.yml

--- script/test-sphinx.sh
+++ script/test-sphinx.sh
@@ -4,9 +4,5 @@ set -o pipefail
 
 echo "Building the documentation of RedPRL with Sphinx..."
 cd doc;
-if [ -n "${TRAVIS}" ]; then
-  make SPHINXOPTS="-n -W" html || exit 1;
-else
-  make html || exit 1;
-fi
+make html || exit 1;
 echo "Done!"
