65fd357f8624d59fb45e9a75e95e6d64f43e8e1c
Carlo Angiuli <carlo@carloangiuli.com>
2019-11-04 16:02:07 -0500

    Fix CI (#698)
    * Fix smlnj CI
    
    * Fix Python SSL issue?
    
    * Keep trying to fix Sphinx???
    
    * Do we even need to install the rtd theme manually?

--- script/test-sphinx.sh
+++ script/test-sphinx.sh
@@ -5,8 +5,6 @@ set -o pipefail
 echo "Building the documentation of RedPRL with Sphinx..."
 cd doc;
 if [ -n "${TRAVIS}" ]; then
-  pip install --user --upgrade requests[security] || exit 1;
-  pip install --user --upgrade Sphinx sphinx-rtd-theme || exit 1;
   make SPHINXOPTS="-n -W" html || exit 1;
 else
   make html || exit 1;
