396aa6c383a2558c451ff075665e189cf0acb875
Carlo Angiuli <carlo@carloangiuli.com>
2019-11-04 15:31:37 -0500

    Fix Python SSL issue?

--- script/test-sphinx.sh
+++ script/test-sphinx.sh
@@ -6,6 +6,7 @@ echo "Building the documentation of RedPRL with Sphinx..."
 cd doc;
 if [ -n "${TRAVIS}" ]; then
   pip install --user --upgrade requests[security] || exit 1;
+  python -m easy_install --upgrade pyOpenSSL || exit 1;
   pip install --user --upgrade Sphinx sphinx-rtd-theme || exit 1;
   make SPHINXOPTS="-n -W" html || exit 1;
 else
