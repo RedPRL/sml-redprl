af4f4364a51400e6cbc4bc11a88b98f135a8fc26
Carlo Angiuli <carlo@carloangiuli.com>
2019-11-04 15:56:11 -0500

    Do we even need to install the rtd theme manually?

--- script/test-sphinx.sh
+++ script/test-sphinx.sh
@@ -5,7 +5,6 @@ set -o pipefail
 echo "Building the documentation of RedPRL with Sphinx..."
 cd doc;
 if [ -n "${TRAVIS}" ]; then
-  pip install --user sphinx-rtd-theme || exit 1;
   make SPHINXOPTS="-n -W" html || exit 1;
 else
   make html || exit 1;
